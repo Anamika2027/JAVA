5fdbbb70cd87b0368f976f52c9271397875f33bc
Anamika2027 <152776977+Anamika2027@users.noreply.github.com>
2025-01-23 22:50:56 +0530

    Update functions.java

--- functions.java
+++ functions.java
@@ -1,51 +1,51 @@
-//problem 1: print given string
-// import java.util.Scanner;
-// public class functions {
-//     public static void printMyName(String name){
-//         System.out.println(name);
-//         return;
-//     }
-//     public static void main(String[] args){
-//         Scanner sc=new Scanner(System.in);
-//         String name=sc.next();
-//         printMyName(name); //function call
-//     }
-// }
+problem 1: print given string
+import java.util.Scanner;
+public class functions {
+    public static void printMyName(String name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        printMyName(name); //function call
+    }
+}
 
-//problem2: calculate sum
-// import java.util.Scanner;
-// public class functions{
-//     public static int printSum(int num1,int num2){
-//         int sum=num1+num2;
-//         return sum;
-//     }
-//     public static void main(String[] args){
-//         Scanner sc=new Scanner(System.in);
-//         int num1=sc.nextInt();
-//         int num2=sc.nextInt();
-//         int sum=printSum(num1,num2);
-//         System.out.println(sum);
-//     }
-// }
+problem2: calculate sum
+import java.util.Scanner;
+public class functions{
+    public static int printSum(int num1,int num2){
+        int sum=num1+num2;
+        return sum;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int sum=printSum(num1,num2);
+        System.out.println(sum);
+    }
+}
 
-//problem3: return the product of given two numbers
+problem3: return the product of given two numbers
 
-// import java.util.Scanner;
-// public class functions{
-//     public static int printProduct(int num1,int num2){
-//         int product=num1*num2;
-//         return product;
-//     }
-//     public static void main(String[] args){
-//         Scanner sc=new Scanner(System.in);
-//         int num1=sc.nextInt();
-//         int num2=sc.nextInt();
-//         int product = printProduct(num1,num2);
-//         System.out.println(product);
-//     }
-// }
+import java.util.Scanner;
+public class functions{
+    public static int printProduct(int num1,int num2){
+        int product=num1*num2;
+        return product;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int product = printProduct(num1,num2);
+        System.out.println(product);
+    }
+}
 
-//problem 4: find the factorial
+problem 4: find the factorial
 
 import java.util.Scanner;
 public class functions{
