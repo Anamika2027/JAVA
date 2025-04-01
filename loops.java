660d0984baaf36e5211f1fd0e39dac0c0e5aaeb6
Anamika2027 <152776977+Anamika2027@users.noreply.github.com>
2025-04-01 21:55:39 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,86 @@
+//for loop
+// public class loops {
+//     public static void main(String args[]){
+//         for(int counter=0; counter<11; counter++){
+//             System.out.print(counter+" ");
+//         }
+//     }
+// }
+
+
+//while loop
+
+// public class loops{
+//     public static void main(String args[]){
+//         int counter=0;
+//         while(counter<11){
+//             System.out.print(counter+" ");
+//             counter++;
+//         }
+//     }
+// }
+
+
+//do while loop
+
+// public class loops{
+//     public static void main(String[] args){
+//         int i=0;
+//         do{
+//             System.out.println(i);
+//             i=i+1;
+//         }while(i<11);
+//     }
+// }
+
+// public class loops{
+//     public static void main(String[] args){
+//         int i=12;
+//         while(i<11){
+//             System.out.println("ac");
+//         }
+
+//         do{
+//             System.out.println("ac");
+//         }while(i<11);
+//     }
+// }
+
+
+//problem1:sum of n=4 natural numbers
+
+// public class loops{
+//     public static void main(String[] args){
+//         int sum=0;
+//         for(int i=1;i<=4;i++){
+//             sum+=i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int sum=0;
+//         for(int i=0;i<=n;i++){
+//             sum=sum+i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+//problem 2: print the table
+
+// import java.util.Scanner;
+// public class loops{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=1;i<=10;i++){
+//             System.out.println(n*i);
+//         }
+//     }
+// }
